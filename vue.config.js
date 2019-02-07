3d366018ab7d2e842c6b1e4a93ca9f99cf6a6c77
minabe <minabe@bringup.jp>
2019-02-07 11:38:07 +0900

    Add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: '/vue_cli/vue_vuetify_1/',
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8888, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
+  runtimeCompiler: true
+}
