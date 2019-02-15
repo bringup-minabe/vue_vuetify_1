d05021719c787f8dc0b97418511df867b0c90cf7
minabe <minabe@bringup.jp>
2019-02-15 11:22:28 +0900

    Add data table compoment

---
+++ src/components/DataTable.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-flex mt-4 mb-4>
+
+    <div class="elevation-1" style="margin-bottom: 40px;">
+      <div class="v-table__overflow">
+        <table class="v-datatable v-table v-datatable--select-all theme--light">
+          <thead>
+            <tr>
+              <td v-if="hide_checkbox === false">
+                <v-checkbox
+                  v-model="selectAll"
+                  primary
+                  hide-details
+                ></v-checkbox>
+              </td>
+              <td v-for="(h, index) in headers" :key="index">
+                {{h.text}}
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in items" :key="index">
+              <td v-if="hide_checkbox === false">
+                <v-checkbox
+                  v-model="selected[index]"
+                  :value="item"
+                  primary
+                  hide-details
+                ></v-checkbox>
+              </td>
+              <td v-for="(h, index) in headers" :key="index">
+                {{item[h.value]}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    headers: Array,
+    items: Array,
+    hide_checkbox: Boolean
+  },
+  data () {
+    return {
+      selected: [],
+      selectAll: false
+    }
+  },
+  watch: {
+    selectAll(val) {
+      if (val) {
+        for (var i = 0; i < this.items.length; i++) {
+          this.selected.push(this.items[i])
+        }
+      } else {
+        this.selected = []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="customers">
     <h1>顧客情報</h1>
+    <DataTable
+    :headers="headers"
+    :items="items"
+    >
+    </DataTable>
   </div>
 </template>
+
+<script>
+import DataTable from "../components/DataTable.vue"
+export default {
+  name: 'customers',
+  components: {DataTable},
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Id',
+          value: 'id'
+        },
+        {
+          text: '氏名',
+          value: 'full_name'
+        },
+        {
+          text: '登録日時',
+          value: 'created'
+        }
+      ],
+      items: [
+        {
+          id: '1',
+          full_name: '山田太郎',
+          created: '2019-02-14 10:20:00'
+        },
+        {
+          id: '2',
+          full_name: '山田次郎',
+          created: '2019-02-15 10:20:00'
+        }
+      ]
+    }
+  }
+}
+</script>
