591cb34db794401b018de49ee209022770313a04
minabe <minabe@bringup.jp>
2019-02-26 16:49:31 +0900

    Update data table

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -333,7 +333,7 @@ export default {
             },
             deep: true
         },
-        page(val) {
+        page() {
             //reset selected
             this.resetSelected()
             //get data
